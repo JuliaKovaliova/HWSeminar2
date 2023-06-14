478d8f65272b9ed482f701c29bb982d23aa7f0e2
Julia <log6117@yandex.ru>
2023-06-14 23:50:07 +0300

    начала вторую задачу

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numb3 (string massage)
+{
+    if (number < 100)
+}
