617b1db045836e0c0db522b3e99954d04623a460
Julia <log6117@yandex.ru>
2023-06-15 12:24:48 +0300

    сделала вторую задачу

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,7 +3,36 @@
 
 Console.WriteLine("Enter number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int numb3 (string massage)
+
+if (number < 0)
 {
-    if (number < 100)
+    number = number * -1;
 }
+
+    if (number < 100)
+    {
+        Console.WriteLine("No third digit!");
+       
+    }
+    else
+    {
+        if (number > 99 && number < 1000)
+        {
+            int result = number % 10;
+            Console.WriteLine("Third digit is " + result);
+        }
+        if (number > 999 && number < 10000)
+        {
+            int result = number / 10 % 10;
+            Console.WriteLine("Third digit is " + result);
+        }
+        if (number > 9999 && number < 100000)
+        {
+            int result = number / 100 % 10;
+            Console.WriteLine("Third digit is " + result);
+        }
+        if (number > 99999)
+        {
+            Console.WriteLine("Please, enter a number up to 99999.");
+        }
+    }
